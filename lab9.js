1d709c6ce33b4de920c732d34a45f9dbc68f63ac
Dofx18362 <dolqfx18362@funix.edu.vn>
2022-12-19 21:48:57 +0700

    948

--- lab9.js
+++ lab9.js
@@ -1,8 +1,47 @@
-var randomEmail = function(domain){
-    var name = Math.random().toString(36).substring(7);
-    var email = name + "@" + domain;
-    return email;
+const data = 
+[
+    {
+    "name": "John Doe",
+    "yearOfEmployment": 1995,
+    "isActive": false
+    },
+    {
+    "name": "Marissa Williams",
+    "yearOfEmployment": 2007,
+    "isActive": true
+    },
+    {
+    "name": "Michile Henry",
+    "yearOfEmployment": 1996,
+    "isActive": true
+    },
+    {
+    "name": "Isabelle Keanna",
+    "yearOfEmployment": 2000,
+    "isActive": true
+    },
+    {
+    "name": "Mark McRolland",
+    "yearOfEmployment": 2018,
+    "isActive": true
+    },
+    {
+    "name": "Shara Lauren",
+    "yearOfEmployment": 1999,
+    "isActive": false
+    },
+    {
+    "name": "Tom ken",
+    "yearOfEmployment": 2001,
+    "isActive": true
+    }
+];
+
+//b1
+var tongnv=function(data){
+    var count=data.length;
+    return count
 }
-    var inputDomain = "gmail.com";
-    var emailAdd = randomEmail(inputDomain);
-    console.log("email:   "+ emailAdd);
+tong=tongnv(data);
+console.log("có: " + tong);
+
